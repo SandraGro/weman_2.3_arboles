1324e65a5381e97e0b6790df87e691b83a5fb856
SandraGro <sandra.guerreroflores@gmail.com>
2019-05-01 17:31:44 -0500

    realiza busqueda binaria y la muestra en HTML

--- js/arboles.js
+++ js/arboles.js
@@ -1,16 +1,34 @@
+var num = null;
+
+
 function binary_search(arr, num) {
-    var end = arr.length;
-    var mid = Math.floor(end/2);
+  var inicio = performance.now();
+  var ctrlMostrado = document.getElementById("texto_encontrado");
+  var ctrlMostrado1 = document.getElementById("inicio");
+  var ctrlMostrado2 = document.getElementById("fin");
+  var ctrlMostrado3 = document.getElementById("final");
+  var num = document.getElementById("txtBuscaR1").value;
+  var end = arr.length;
+  var mid = Math.floor(end / 2);
   
-    if(end == 0){
-      return -1;
-    }
-    
-    if(num == arr[mid].id){
-      return arr[mid];
-    }else if(num > arr[mid].id){
-      return binary_search(arr.slice(mid+1, end), num);
-    }else{
-      return binary_search(arr.slice(0, mid), num);
-    }
+  if (end == 0) {
+    return -1;
   }
+  
+  if (num == arr[mid].id) {
+  } else if (num > arr[mid].id) {
+    return binary_search( arr.slice(mid + 1, end), num);
+  } else {
+    return binary_search( arr.slice(0, mid), num);
+  }
+  
+  var fin= performance.now();
+  var tiempo_final = fin-inicio;
+  ctrlMostrado.innerHTML = arr[mid].nombre;
+  ctrlMostrado1.innerHTML = inicio;
+  ctrlMostrado2.innerHTML = fin;
+  ctrlMostrado3.innerHTML = tiempo_final;
+
+  console.log(tiempo_final);
+}
+ 
